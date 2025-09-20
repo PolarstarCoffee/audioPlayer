55f501ae8ce3447b8303aa8862533ed0188c76e5
Zuriel Zavala <zzavala2@hotmail.com>
2025-09-20 15:52:22 -0500

    clean up

--- src/app.java
+++ src/app.java
@@ -8,12 +8,6 @@ public class app {
             @Override
             public void run() {
                 new musicPlayerGUI().setVisible(true);
-
-                // Song song = new Song(
-                // "src/assets/Otorii Station - Chill 🌴 Marvel vs. Capcom 2- New Age of Heroes
-                // 🥋 - 04 Airship Stage.mp3");
-                // System.out.println("Title: " + song.getSongTitle());
-                // System.out.println("Artist: " + song.getSongArtist());
             }
         });
     }
