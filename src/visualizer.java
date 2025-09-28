2de32a3b8e956fd039f010516670e0a2dde3331a
Zuriel Zavala <zzavala2@hotmail.com>
2025-09-28 18:39:13 -0500

    Correct framework addition

--- src/visualizer.java
+++ src/visualizer.java
@@ -1,6 +1,10 @@
 package src;
 
-import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.*;
+
+// Placeholder for visualizer class
+
+// Currently empty, to be implemented later
 
 public class visualizer {
 
